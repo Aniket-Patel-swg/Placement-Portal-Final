c0b8fede8383a0a31b25cc0241850621837ba777
Vraj Shah <svraj073@gmail.com>
2023-01-22 12:46:29 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#1F3368",
+        orange: "#F68C1F",
+        white: "#F2F2F1",
+      },
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      boxShadow: {
+        '3xl': '0 4px 30px 10px rgba(0, 0, 0, 0.5)',
+      }
+    },
   },
+ 
   plugins: [],
 }
